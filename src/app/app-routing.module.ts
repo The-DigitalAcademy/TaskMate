48044833f0a224e655d1863c28cfde5ead213b28
kgalalelo <lefyedik@gmail.com>
2025-11-12 15:47:22 +0200

    added styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TaskManagementComponent } from './task-management/task-management.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'task-management' , component: TaskManagementComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],

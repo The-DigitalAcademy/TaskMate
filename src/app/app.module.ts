ea9045c54c80eccee6509294550734d3f09ebba8
kgalalelo <lefyedik@gmail.com>
2025-11-12 10:46:19 +0200

    created filters component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './feature/header/header.component';
+import { FiltersComponent } from './filters/filters.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
